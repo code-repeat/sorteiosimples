e9d45326403df751b1b4547c57f4f9edf7812503
Code Repeat <coderepeat44@gmail.com>
2020-07-01 15:23:29 -0300

    Refatoração: força ao usuário apenas digitar números inteiros na primeira pergunta que o programa faz

--- sorteio.py
+++ sorteio.py
@@ -1,19 +1,31 @@
-#  Módulo(s) necessário(s)
+#  importing modules
 from random import choices
 
-#  Capa do Programa
+#  Program cover
 print("*"*50)
 print("-------------SORTEIO SIMPLES----------------------")
 print("*"*50)
 
-quant_names = int(input("Quantos itens deseja adcionar na caixa de sorteio? "))
+#  Verify if the user only types a integer number
+quant_names = ''
+while type(quant_names) != int:
+    print("Quantos nomes você deseja adcionar na lista? ")
+    quant_names = input()
+    try:
+        int(quant_names)
+        break
+    except ValueError:
+        print("SOMENTE NÚMEROS")
+
 box_itens = []
 
-for i in range(quant_names):
+#  This code below runs according to the user answer
+for i in range(int(quant_names)):
     item = input(f"Item {i}: ")
     box_itens.append(item)
 
 draw_numbers = int(input("Quantos itens você deseja sortear? "))
+
 if draw_numbers > len(box_itens):
     print("Digite um valor menor")
 else:
